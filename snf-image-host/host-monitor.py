453ab96555cfafd9b06c58cb725704c14441ba65
Nikos Skalkotos <skalkoto@grnet.gr>
2012-07-23 16:22:25 +0300

    Create a dedicated host monitoring program
    This program is used to output info and error messages from the host.

--- snf-image-host/host-monitor.py
+++ snf-image-host/host-monitor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2012 GRNET S.A. 
+# Copyright (C) 2012 GRNET S.A.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,25 +17,55 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301, USA.
 
+"""Utility that generates monitoring messages for snf-image.
+
+This utility given a message type as option and the message body as input will
+print a monitoring message in stdout.
+"""
+
 import sys
 import os
 import json
 import time
 
+MSG_TYPE_ERROR = "image-error"
+MSG_TYPE_INFO = "image-info"
+
+PROTOCOL = {
+    "error": (MSG_TYPE_ERROR, "messages"),
+    "stderr": (MSG_TYPE_ERROR, "stderr"),
+    "info": (MSG_TYPE_INFO, "messages")
+}
+
 PROGNAME = os.path.basename(sys.argv[0])
 
 if __name__ == "__main__":
-    usage="Usage: %s <msg-type>\n" % PROGNAME
-    
+    usage = "Usage: %s <msg-type>\n" % PROGNAME
+
     if len(sys.argv) != 2:
         sys.stderr.write(usage)
         sys.exit(1)
 
-    msg = {}
-    msg['type'] =  sys.argv[1]
-    msg['stderr'] = sys.stdin.read()
-    msg['timestamp'] = time.time()
+    msg_type = sys.argv[1]
 
+    if msg_type  not in PROTOCOL.keys():
+        sys.stderr.write("Unknown message type: %s\n" % msg_type)
+        sys.exit(1)
+
+    msg = {}
+    msg['type'] = PROTOCOL[msg_type][0]
+
+    lines = []
+    while True:
+        line = sys.stdin.readline()
+
+        if not line:
+            break
+
+        lines.append(line.strip())
+
+    msg[PROTOCOL[msg_type][1]] = lines
+    msg['timestamp'] = time.time()
     sys.stdout.write("%s\n" % json.dumps(msg))
 
     sys.exit(0)
