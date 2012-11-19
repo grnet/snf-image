grnet/snf-image

commit 9fca5245a2eae67af86719bb0ff885039bdbc7e8

Don't overwrite C:\Unattend.xml in the windows
If unattend.xml is present in the windows image, use this one unless
USE_DEFAULT_UNATTEND is specified as an image property.

snf-image-helper/tasks/40InstallUnattend.in
@@ -45,15 +45,23 @@ fi
 target=$SNF_IMAGE_TARGET
 mkdir -p "$target/Windows/Setup/Scripts"
 
-if [ -n "$SNF_IMAGE_UNATTEND" ]; then
-    echo "Installing custom Unattend.xml file..."
-    if [ -f "$SNF_IMAGE_UNATTEND" ]; then
-        cat "$SNF_IMAGE_UNATTEND" > "$target/Unattend.xml"
-    else
-        log_error "Custom unattend file: \`"$SNF_IMAGE_UNATTEND"' is missing"
-    fi
+# Workaround to search for C:\Unattend.xml in an case insensitive way.
+exists=$(find "$target"/ -maxdepth 1 -iname unattend.xml)
+
+if [ -n "$exists" -a -z "$SNF_IMAGE_PROPERTY_USE_DEFAULT_UNATTEND" ]; then
+    warn "Using the Unattend.xml file found in the image"
 else
-    cat "@commondir@/unattend.xml" > "$target/Unattend.xml"
+    xargs rm -f <<< "$exists"
+    if [ -n "$SNF_IMAGE_UNATTEND" ]; then
+        echo "Installing custom Unattend.xml file..."
+        if [ -f "$SNF_IMAGE_UNATTEND" ]; then
+            cat "$SNF_IMAGE_UNATTEND" > "$target/Unattend.xml"
+        else
+            log_error "Custom unattend file: \`"$SNF_IMAGE_UNATTEND"' is missing"
+        fi
+    else
+        cat "@commondir@/unattend.xml" > "$target/Unattend.xml"
+    fi
 fi
 
 echo "del /Q /F C:\Unattend.xml" > \

commit aef6b35bf9dd1c311c0dc94dde8d440f22d5ed23

Fix a bug introduced in 9fca5245a2eae67af86719bb0f
AssignHostname task did not search for Unattend.xml in a case
insensitive way.

snf-image-helper/common.sh
@@ -335,6 +335,18 @@ get_last_free_sector() {
     fi
 }
 
+get_unattend() {
+    local target="$1"
+
+    # Workaround to search for $target/Unattend.xml in an case insensitive way.
+    exists=$(find "$target"/ -maxdepth 1 -iname unattend.xml)
+    if [ $(wc -l <<< "$exists") -gt 1 ]; then
+        log_error "Found multiple Unattend.xml files in the image:" $exists
+    fi
+
+    echo "$exists"
+}
+
 cleanup() {
     # if something fails here, it shouldn't call cleanup again...
     trap - EXIT

snf-image-helper/tasks/40InstallUnattend.in
@@ -45,13 +45,12 @@ fi
 target=$SNF_IMAGE_TARGET
 mkdir -p "$target/Windows/Setup/Scripts"
 
-# Workaround to search for C:\Unattend.xml in an case insensitive way.
-exists=$(find "$target"/ -maxdepth 1 -iname unattend.xml)
+unattend=$(get_unattend "$target")
 
-if [ -n "$exists" -a -z "$SNF_IMAGE_PROPERTY_USE_DEFAULT_UNATTEND" ]; then
+if [ -n "$unattend" -a -z "$SNF_IMAGE_PROPERTY_USE_DEFAULT_UNATTEND" ]; then
     warn "Using the Unattend.xml file found in the image"
 else
-    xargs rm -f <<< "$exists"
+    rm -f "$unattend"
     if [ -n "$SNF_IMAGE_UNATTEND" ]; then
         echo "Installing custom Unattend.xml file..."
         if [ -f "$SNF_IMAGE_UNATTEND" ]; then

snf-image-helper/tasks/50AssignHostname.in
@@ -37,16 +37,21 @@ windows_hostname() {
     local target="$1"
     local password="$2"
 
-    local tmp_unattend=`mktemp` || exit 1
+    local tmp_unattend=$(mktemp) || exit 1
     add_cleanup rm "$tmp_unattend"
 
     echo -n "Assigning new computer name..."
 
     local namespace="urn:schemas-microsoft-com:unattend"
-    
-    "$XMLSTARLET" ed -N x=$namespace -u "/x:unattend/x:settings/x:component/x:ComputerName" -v "$password" "$target/Unattend.xml" > "$tmp_unattend"
 
-    cat "$tmp_unattend" > "$target/Unattend.xml"
+    unattend=$(get_unattend "$target")
+    if [ -z "$unattend" ]; then
+        log_error "Unattend.xml is missing."
+    fi
+    
+    "$XMLSTARLET" ed -N x=$namespace -u "/x:unattend/x:settings/x:component/x:ComputerName" -v "$password" "$unattend" > "$tmp_unattend"
+
+    cat "$tmp_unattend" > "$unattend"
     echo done
 }
 