8056863b7499be0f676f334c0d38eb3ff86edc3a
Dimitris Aragiorgis <dimitris.aragiorgis@gmail.com>
2015-07-07 17:46:21 +0300

    kvm: Support various target device types
    Old QEMU versions used -drive file=<file>,iface=<iface> in order
    to define a virtual disk. Until now snf-image was using this
    deprecated method and launched the helper using the virtio
    interface. Here we use the new interface which goes down to:
    
     -drive file=<file>,iface=none,id=disk0
     -device <driver>,drive=disk0
    
    Ganeti exports the disk type to be used as INSTANCE_HV_disk_type.
    Some of the allowed values are: paravirtual, scsi, scsi-hd,
    scsi-generic, scsi-block, etc.
    
    In case of paravirtual device type the <driver> will be
    "virtio-blk-pci" and the helper will see the disk as /dev/vdb.
    
    If the disk type is scsi'ish then the helper will see the device as
    /dev/sda and the <driver> will be:
    
      - scsi-hd for scsi
      - the same as disk type otherwise
    
    Note that in case of a scsi'ish disk type an extra SCSI controller
    (virtio-scsi-pci) is added to the QEMU command line.
    
    Signed-off-by: Dimitris Aragiorgis <dimitris.aragiorgis@gmail.com>

--- snf-image-host/kvm-common.sh
+++ snf-image-host/kvm-common.sh
@@ -15,16 +15,38 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301, USA.
 
-assign_disk_devices_to() {
-    local varname
-    varname="$1"
+get_img_dev() {
+    local idx=$1
+    local letters=( a b c d e f g h e j k l m n )
 
+    case $disk_type in
+        # The helper's root disk is always paravirtual and thus /dev/vda
+        # will be occupied.
+        paravirtual) echo /dev/vd${letters[$idx+1]};;
+        scsi-generic|scsi-block|scsi-hd|scsi) echo /dev/sd${letters[$idx]};;
+    esac
+}
+
+get_img_driver() {
+    case $disk_type in
+        paravirtual) echo virtio-blk-pci;;
+        scsi|scsi-hd) echo scsi-hd;;
+        scsi-block) echo scsi-block;;
+        scsi-generic) echo scsi-generic;;
+    esac
+}
+
+assign_disk_devices_to() {
+    local varname="$1"
+
+    # This will declare the given variable as an array
     eval $varname=\(\)
 
-    set -- b c d e f g h e j k l m n
-
+    # For DISK_COUNT=3, disk_type=paravirtual, and varname=snf_export_DEV
+    # this will create snf_export_DEV array of ( /dev/vdb /dev/vdc /dev/vdd )
     for ((i = 0; i < DISK_COUNT; i++)); do
-        eval $varname+=\(\"/dev/vd$1\"\); shift
+        dev=$(get_img_dev $i)
+        eval $varname+=\(\"$dev\"\)
     done
 }
 
@@ -41,7 +63,8 @@ launch_helper() {
 
     disks=""
     for ((i=0; i < DISK_COUNT; i++)); do
-        disks+=" -drive file=$(find_disk $i),format=raw,if=virtio,cache=none"
+        disks+=" -drive file=$(find_disk $i),format=raw,if=none,cache=none,id=drive$i"
+        disks+=" -device $(get_img_driver),id=disk$i,drive=drive$i"
     done
 
     set +e
@@ -52,11 +75,16 @@ launch_helper() {
         HELPER_DEBUG_ARG=""
     fi
 
+    if [[ "$disk_type" =~ ^scsi ]]; then
+        EXTRA_CONTROLLER_ARG="-device virtio-scsi-pci"
+    fi
 
     $TIMEOUT -k "$HELPER_HARD_TIMEOUT" "$HELPER_SOFT_TIMEOUT" \
       $KVM -runas "$HELPER_USER" \
-      -drive file="$HELPER_DIR/image",format=raw,if=virtio,readonly \
-      $disks -m "$HELPER_MEMORY" -boot c -serial stdio \
+      -drive file="$HELPER_DIR/image",format=raw,if=none,id=helper,readonly \
+      -device virtio-blk-pci,id=helper,drive=helper \
+      $EXTRA_CONTROLLER_ARG $disks \
+      -m "$HELPER_MEMORY" -boot c -serial stdio \
       -serial "file:$(printf "%q" "$result_file")" \
       -serial file:>(./helper-monitor.py ${MONITOR_FD}) \
       -serial pty \
