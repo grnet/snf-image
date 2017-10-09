fb15043555a776651eef5103d5e44ab708279a26
Nikos Skalkotos <skalkoto@grnet.gr>
2017-10-09 12:24:13 +0300

    Use splice() to transfer data btw back-ends
    Till now, the copy-monitor.py helper script expected an program
    (e.g. dd) as argument, it would execute it as child process and monitor
    the bytes read by it. This worked well because the whole image copying
    was performed in a single step.
    
    Now that we pipe the output of the source back-end to the input of the
    destination back-end, doing the image copy like this:
    
    src | copy-monitor.py dd | dst
    
    is inefficient. The pipe buffer is by default 64K and no matter how you
    set the size of dd's input and output buffers, the copy is performed in
    reduced speed.
    
    To overcome this, instead of using an external program, make
    copy-monitor.py move the input data to output using splice() after
    growing the pipe capacity.

--- snf-image-host/copy-monitor.py
+++ snf-image-host/copy-monitor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2011, 2012 GRNET S.A.
+# Copyright (C) 2011, 2012, 2017 GRNET S.A.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,8 +19,7 @@
 
 """Utility to monitor the progress of image deployment
 
-A small utility to monitor the progress of image deployment
-by watching the contents of /proc/<pid>/io and producing
+A small utility to monitor the progress of image deployment and produce
 notifications.
 """
 
@@ -28,131 +27,174 @@ import os
 import sys
 import time
 import json
-import prctl
 import signal
-import socket
+import argparse
+import errno
+import ctypes
+import ctypes.util
+import fcntl
+from functools import partial
 
 MSG_TYPE = "image-copy-progress"
 
+# From bits/fcntl-linux.h
+SPLICE_F_MOVE = 1
+SPLICE_F_NONBLOCK = 2
+SPLICE_F_MORE = 4
+SPLICE_F_GIFT = 8
 
-def parse_arguments(args):
-    from optparse import OptionParser
+# From linux/fcntl.h
+F_SETPIPE_SZ = 1031
 
-    kw = {}
-    kw['usage'] = "%prog [options] command [args...]"
-    kw['description'] = \
-        "%prog runs 'command' with the specified arguments, monitoring the " \
-        "number of bytes read by it. 'command' is assumed to be " \
-        "A program used to install the OS for a Ganeti instance. %prog " \
-        "periodically issues notifications of type 'copy-progress'."
 
-    parser = OptionParser(**kw)
-    parser.disable_interspersed_args()
-    parser.add_option("-r", "--read-bytes",
-                      action="store", type="int", dest="read_bytes",
-                      metavar="BYTES_TO_READ",
-                      help="The expected number of bytes to be read, "
-                           "used to compute input progress",
-                      default=None)
-    parser.add_option(
-        "-o", "--output_fd", dest="output", default=None, metavar="FILE",
-        type="int", help="Write output notifications to this file descriptor")
+def make_splice():
+    '''Set up a splice(2) wrapper'''
+    # Load libc
+    libc = ctypes.CDLL(ctypes.util.find_library('c'), use_errno=True)
 
-    (opts, args) = parser.parse_args(args)
+    # Get a handle to the 'splice' call
+    c_splice = libc.splice
+    del libc
 
-    if opts.read_bytes is None:
-        sys.stderr.write("Fatal: Option '-r' is mandatory.\n")
+    c_splice.argtypes = [
+        ctypes.c_int, ctypes.POINTER(ctypes.c_longlong),
+        ctypes.c_int, ctypes.POINTER(ctypes.c_longlong),
+        ctypes.c_size_t,
+        ctypes.c_uint
+    ]
+    c_splice.restype = ctypes.c_ssize_t
+
+    # pylint: disable=redefined-outer-name
+    def splice(fd_in, fd_out, len_, flags):
+        '''Wrapper for splice(2)
+
+        If the call to `splice` fails (i.e. returns -1), an `OSError` is raised
+        with the appropriate `errno`, unless the error is `EINTR`, which
+        results in the call to be retried.
+        '''
+
+        while True:
+            res = c_splice(fd_in, None, fd_out, None, len_, flags)
+
+            if res == -1:
+                errno_ = ctypes.get_errno()
+
+                # Try again on EINTR
+                if errno_ == errno.EINTR:
+                    continue
+
+                raise IOError(errno_, os.strerror(errno_))
+
+            return res
+
+    return splice
+
+
+# Build and export wrapper
+splice = make_splice()  # pylint: disable=invalid-name
+del make_splice
+
+
+class Progress(object):
+    """Computes the progress made"""
+    def __init__(self, out, interval, start, total):
+        self.out = out
+        self.interval = interval
+        self.position = start
+        self.msg = {"type": MSG_TYPE, "total": total}
+        signal.signal(signal.SIGALRM, partial(self.send_progress, self))
+        signal.alarm(self.interval)
+
+    def update(self, val):
+        """Update the current progress"""
+        self.position += val
+
+    def send_progress(self, *_):
+        """Send progress to file descriptor"""
+        self.msg['position'] = self.position
+        self.msg['progress'] = float(0) if self.msg["total"] == 0 else \
+            float("%2.2f" % (self.position * 100.0 / self.msg['total']))
+        self.msg['timestamp'] = time.time()
+        os.write(self.out, "%s\n" % json.dumps(self.msg))
+
+        signal.alarm(self.interval)
+
+
+def parse_arguments():
+    """Parse input arguments"""
+    description = \
+        "Monitor the data passed through a pipe and periodically issue " \
+        "notifications of type 'copy-progress'"
+
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        "-b", "--buffer-size", type=int, dest="buffer_size", default=65536,
+        metavar="BYTES", help="Transfer up to BYTES in every iteration")
+    parser.add_argument(
+        "-i", "--interval", dest="interval", default=3, type=int,
+        help="The copy-progress messages creation interval")
+    parser.add_argument(
+        "-o", "--output_fd", type=int, dest="out", default=None, metavar="FD",
+        help="Write output notifications to this file descriptor")
+    parser.add_argument(
+        "-s", "--start", type=int, dest="start", default=0,
+        help="The number of bytes already transferred")
+    parser.add_argument(
+        "-t", "--total", type=int, dest="total", default=None, metavar="TOTAL",
+        help="The overall number of bytes expected to be transferred")
+
+    args = parser.parse_args()
+
+    if args.total is None:
+        sys.stderr.write("Fatal: Option '-t' is mandatory.\n")
         parser.print_help()
         sys.exit(1)
 
-    if opts.output is None:
+    if args.out is None:
         sys.stderr.write("Fatal: Option '-o' is mandatory.\n")
         parser.print_help()
         sys.exit(1)
 
-    if len(args) == 0:
-        sys.stderr.write("Fatal: You need to specify the command to run.\n")
-        parser.print_help()
-        sys.exit(1)
-
-    return (opts, args)
-
-
-def report_wait_status(pid, status):
-    if os.WIFEXITED(status):
-        sys.stderr.write("Child PID = %d exited, status = %d\n" %
-                         (pid, os.WEXITSTATUS(status)))
-    elif os.WIFSIGNALED(status):
-        sys.stderr.write("Child PID = %d died by signal, signal = %d\n" %
-                         (pid, os.WTERMSIG(status)))
-    elif os.WIFSTOPPED(status):
-        sys.stderr.write("Child PID = %d stopped by signal, signal = %d\n" %
-                         (pid, os.WSTOPSIG(status)))
-    else:
-        sys.stderr.write("Internal error: Unhandled case, "
-                         "PID = %d, status = %d\n" % (pid, status))
-        sys.exit(1)
-    sys.stderr.flush()
-
-
-def send_message(to, message):
-    message['timestamp'] = time.time()
-    os.write(to, "%s\n" % json.dumps(message))
+    return args
 
 
 def main():
-    (opts, args) = parse_arguments(sys.argv[1:])
-    out = opts.output
-    pid = os.fork()
-    if pid == 0:
-        # In child process:
+    """ module entry point"""
+    args = parse_arguments()
 
-        # Make sure we die with the parent and are not left behind
-        # WARNING: This uses the prctl(2) call and is Linux-specific.
-        prctl.set_pdeathsig(signal.SIGHUP)
+    if os.isatty(sys.stdin.fileno()):
+        sys.stderr.write("Input is a tty. Expecting a pipe!\n")
+        return 2
 
-        # exec command specified in arguments,
-        # searching the $PATH, keeping all environment
-        os.execvpe(args[0], args, os.environ)
-        sys.stderr.write("execvpe failed, exiting with non-zero status")
-        os.exit(1)
+    if os.isatty(sys.stdout.fileno()):
+        sys.stderr.write("Output is a tty. Expecting a pipe!\n")
+        return 2
 
-    # In parent process:
-    iofname = "/proc/%d/io" % pid
-    iof = open(iofname, "r", 0)   # 0: unbuffered open
-    sys.stderr.write("%s: created child PID = %d, monitoring file %s\n" %
-                     (sys.argv[0], pid, iofname))
+    with open('/proc/sys/fs/pipe-max-size') as pipe_max_size:
+        max_size = pipe_max_size.read()
 
-    message = {}
-    message['type'] = MSG_TYPE
-    message['total'] = opts.read_bytes
+    if max_size < args.buffer_size:
+        args.buffer_size = max_size
+
+    # Make the pipe size equal to the chunk size
+    fcntl.fcntl(sys.stdin.fileno(), F_SETPIPE_SZ, args.buffer_size)
+    fcntl.fcntl(sys.stdout.fileno(), F_SETPIPE_SZ, args.buffer_size)
+
+    progress = Progress(args.out, args.interval, args.start, args.total)
 
     while True:
-        # check if the child process is still alive
-        (wpid, status) = os.waitpid(pid, os.WNOHANG)
-        if wpid == pid:
-            report_wait_status(pid, status)
-            if (os.WIFEXITED(status) or os.WIFSIGNALED(status)):
-                if not (os.WIFEXITED(status) and os.WEXITSTATUS(status) == 0):
-                    return 1
-                else:
-                    message['position'] = message['total']
-                    message['progress'] = float(100)
-                    send_message(out, message)
-                    return 0
+        sent = splice(sys.stdin.fileno(), sys.stdout.fileno(),
+                      args.buffer_size, SPLICE_F_MOVE)
+        if sent < 0:
+            return 3
+        elif sent == 0:
+            break
+        else:
+            progress.update(sent)
 
-        iof.seek(0)
-        for l in iof.readlines():
-            if l.startswith("rchar:"):
-                message['position'] = int(l.split(': ')[1])
-                message['progress'] = float(0) if opts.read_bytes == 0 \
-                    else float("%2.2f" % (
-                        message['position'] * 100.0 / message['total']))
-                send_message(out, message)
-                break
+    progress.send_progress()
+    return 0
 
-        # Sleep for a while
-        time.sleep(3)
 
 if __name__ == "__main__":
     sys.exit(main())
